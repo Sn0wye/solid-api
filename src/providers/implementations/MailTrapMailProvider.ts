ce8960490d18847eb341a5ca371716b08d175255
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-08-23 17:31:04 -0300

    feat: add email implementation

--- src/providers/implementations/MailTrapMailProvider.ts
+++ src/providers/implementations/MailTrapMailProvider.ts
@@ -1,11 +1,33 @@
 import nodemailer from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
 import { IMailProvider, IMessage } from '../IMailProvider';
 
 export class MailTrapMailProvider implements IMailProvider {
-  private transporter;
+  private transporter: Mail;
   constructor() {
-    this.transporter = nodemailer.createTransport({});
+    this.transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      service: 'gmail',
+      auth: {
+        user: process.env.NODEMAILER_USER,
+        pass: process.env.NODEMAILER_PASSWORD,
+      },
+    });
   }
 
-  async sendMail(message: IMessage): Promise<void> {}
+  async sendMail(message: IMessage): Promise<void> {
+    await this.transporter.sendMail({
+      to: {
+        name: message.to.name,
+        address: message.to.email,
+      },
+      from: {
+        name: message.from.name,
+        address: message.from.email,
+      },
+      subject: message.subject,
+      text: message.body,
+    });
+  }
 }
